0a1aaa57d20992025978a1d509de6bb78967d904
Evandro Murilo <evandromurilob@hotmail.com>
2017-05-15 13:40:08 -0400

    add layout integration test

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,19 +14,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get help" do
     get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", full_title("Help")
   end
   
   test "should get about" do
     get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", full_title("About")
   end
   
   test "should get contact" do
     get contact_path
     assert_response :success
-    assert_select "title", "Contact | #{@base_title}"
+    assert_select "title", full_title("Contact")
   end
 
 end
