51af39df6aca77f6a88f1bf92ca175812dc256f2
Evandro Murilo <evandromurilob@hotmail.com>
2017-05-15 14:56:12 -0400

    add User Controller

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,3 +1,5 @@
+require 'test_helper'
+
 class SiteLayoutTest < ActionDispatch::IntegrationTest
     test "layout links" do
         get root_path
@@ -6,5 +8,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
         assert_select "a[href=?]", help_path
         assert_select "a[href=?]", about_path
         assert_select "a[href=?]", contact_path
+        get contact_path
+        assert_select "title", full_title("Contact")
+        get signup_path
+        assert_select "title", full_title("Sign up")
     end
 end
