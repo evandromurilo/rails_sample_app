3ca3add04c07365cf39aee3498ffde360048989f
Evandro Murilo <evandromurilob@hotmail.com>
2017-05-17 14:34:36 +0000

    improve signup integration test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -16,4 +16,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         assert_select 'div#error_explanation ul li', 4
         assert_select 'div.field_with_errors', 8
     end
+    
+    test "valid signup information" do
+        get signup_path
+        assert_difference 'User.count', 1 do
+            assert_select 'form[action="/signup"]'
+            post signup_path, params: { user: { name: "Evandro Murilo",
+                                                email: "evandromurilob@hotmail.com",
+                                                password: "12345678",
+                                                password_confirmation: "12345678" }}
+        end
+        follow_redirect!
+        assert_template 'users/show'
+        
+        assert_select 'div.alert-success'
+    end
 end
