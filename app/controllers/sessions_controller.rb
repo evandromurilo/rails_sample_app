1d327d49f0ceccdda2e9d9d7da0cc1fec3c829c8
Evandro Murilo <evandromurilob@hotmail.com>
2017-05-18 09:01:48 -0400

    add login error messages and basic integration test

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,5 +3,20 @@ class SessionsController < ApplicationController
     end
     
     def create
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user == nil
+            flash.now[:danger] = "Email not found."
+            render 'new'
+        elsif !user.authenticate(params[:session][:password])
+            flash.now[:danger] = "Wrong password."
+            render 'new'
+        else
+            flash[:success]= "Nice!"
+            redirect_to user
+        end
+
+    end
+
+    def destroy
     end
 end
