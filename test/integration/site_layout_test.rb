4aa289b18719ba5ac36e5891afcbd129a4e6e451
Evandro Murilo <evandromurilob@hotmail.com>
2017-05-22 19:05:57 -0400

    revamp users index page and layout integration test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+    def setup
+        @user = users(:nathalya)
+        @other_user = users(:beatriz)
+    end
+    
     test "layout links" do
         get root_path
         assert_template 'static_pages/home'
@@ -12,5 +17,29 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
         assert_select "title", full_title("Contact")
         get signup_path
         assert_select "title", full_title("Sign up")
+        get about_path
+        assert_select "title", full_title("About")
+    end
+
+    test "layout links for logged-in user" do
+        log_in_as(@user, password: "12345678")
+        get root_path
+        assert_select "a[href=?]", root_path, count: 2
+        assert_select "a[href=?]", help_path
+        assert_select "a[href=?]", about_path
+        assert_select "a[href=?]", contact_path
+        assert_select "a[href=?]", users_path
+        assert_select "a[href=?]", user_path(@user)
+        assert_select "a[href=?]", logout_path
+        assert_select "a[href=?]", edit_user_path(@user)
+        get edit_user_path(@other_user)
+        assert_redirected_to root_path
+        get edit_user_path(@user)
+        assert_template 'users/edit'
+        assert_select "title", full_title("Edit user")
+        get users_path
+        assert_select "title", full_title("All users")
+        get user_path(@user)
+        assert_select "title", full_title(@user.name)
     end
 end
